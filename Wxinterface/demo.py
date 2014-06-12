189152e962dcdf41df652243e01f960ee8b37d9f
shamcle <siweidashu@gmail.com>
2014-06-13 02:33:14 +0800

    update

--- Wxinterface/demo.py
+++
@@ -1,7 +0,0 @@
-#coding=utf-8
-from WxInterface import WxClient
-
-config = {"appid":"wx0cb6649ff52bba7d", "secret":"21018fb1693aeda3fe5d62bcaabb8426"}
-
-client = WxClient(config)
-print client.getgroup()
